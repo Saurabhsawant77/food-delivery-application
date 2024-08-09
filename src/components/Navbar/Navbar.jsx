ab82bafb9e76434de96ab64bdda73e80dfffeeaa
Saurabh Sawant <113126470+Saurabhsawant77@users.noreply.github.com>
2024-08-09 21:33:29 +0530

    Completed Project Final touches are done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,36 +1,61 @@
-import React, { useContext, useState } from 'react'
-import './Navbar.css'
-import { assets } from '../../assets/assets'
-import { Link } from 'react-router-dom'
-import { StoreConstext } from '../../context/StoreContext'
+import React, { useContext, useState } from 'react';
+import './Navbar.css';
+import { assets } from '../../assets/assets';
+import { Link } from 'react-router-dom';
+import { StoreConstext } from '../../context/StoreContext';
+import { HashLink } from 'react-router-hash-link'; // Import HashLink
 
-const Navbar = ({setShowLogin}) => {
-
-    const [menu,setMenu] = useState("home");
-
-    const {getTotalCartAmount} = useContext(StoreConstext);
-
+const Navbar = ({ setShowLogin }) => {
+  const [menu, setMenu] = useState("home");
+  const { getTotalCartAmount } = useContext(StoreConstext);
 
   return (
-    <div className='navbar' >
-       <Link to='/food-delivery-application/'><img src={assets.logo} alt="" className="logo" /></Link> 
-        <ul className="navbar-menu">
-            <Link to='/food-delivery-application/' onClick={() => setMenu("home")} className={menu==="home" ? "active" :"" }>Home</Link>
-            <a href='#explore-menu'  onClick={() => setMenu("menu")} className={menu==="menu"? "active" :""}>Menu </a>
-
-          <a href='#app-download' onClick={() => setMenu("mobile-app")} className={menu==="mobile-app"? "active" :""}>Mobile App</a> 
-            <a href='#footer' onClick={() => setMenu("contact-us")} className={menu==="contact-us"? "active" :""}>Contact us</a>
-        </ul>
-        <div className="navbar-right">
-            <img src={assets.search_icon} alt="" srcset="" />
-            <div className="navbar-searchicon">
-               <Link to='/food-delivery-application/cart' > <img src={assets.basket_icon} alt="" srcset="" /> </Link>
-                <div className={getTotalCartAmount()===0?"":"dot"}></div>
-            </div>
-            <button onClick={()=>setShowLogin(true)}>sign in</button>
+    <div className='navbar'>
+      <Link to='/food-delivery-application/'>
+        <img src={assets.logo} alt="Logo" className="logo" />
+      </Link>
+      <ul className="navbar-menu">
+        <Link 
+          to='/food-delivery-application/' 
+          onClick={() => setMenu("home")} 
+          className={menu === "home" ? "active" : ""}
+        >
+          Home
+        </Link>
+        <HashLink 
+          to='/food-delivery-application/#explore-menu' 
+          onClick={() => setMenu("menu")} 
+          className={menu === "menu" ? "active" : ""}
+        >
+          Menu
+        </HashLink>
+        <HashLink 
+          to='/food-delivery-application/#app-download' 
+          onClick={() => setMenu("mobile-app")} 
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          Mobile App
+        </HashLink>
+        <HashLink 
+          to='/food-delivery-application/#footer' 
+          onClick={() => setMenu("contact-us")} 
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          Contact us
+        </HashLink>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="Search" />
+        <div className="navbar-searchicon">
+          <Link to='/food-delivery-application/cart'>
+            <img src={assets.basket_icon} alt="Cart" />
+          </Link>
+          <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
         </div>
+        <button onClick={() => setShowLogin(true)}>Sign In</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,9 +1,12 @@
-import React, { useContext } from 'react'
-import './PlaceOrder.css'
-import { StoreConstext } from '../../context/StoreContext'
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './PlaceOrder.css';
+import { StoreConstext } from '../../context/StoreContext';
 
 const PlaceOrder = () => {
-  const {getTotalCartAmount} = useContext(StoreConstext)
+  const { getTotalCartAmount } = useContext(StoreConstext);
+  const navigate = useNavigate(); // Use the useNavigate hook here
+
   return (
     <form className='place-order'>
       <div className="place-order-left">
@@ -11,23 +14,23 @@ const PlaceOrder = () => {
           Delivery Information
         </p>
         <div className="multi-fields">
-          <input type="text" placeholder='First Name'/>
-          <input type="text" placeholder='Last Name'/>
+          <input type="text" placeholder='First Name' />
+          <input type="text" placeholder='Last Name' />
         </div>
-        <input type="email" placeholder='Email Address'/>
-        <input type="text" placeholder='Street'/>
+        <input type="email" placeholder='Email Address' />
+        <input type="text" placeholder='Street' />
         <div className="multi-fields">
-          <input type="text" placeholder='City'/>
-          <input type="text" placeholder='State'/>
+          <input type="text" placeholder='City' />
+          <input type="text" placeholder='State' />
         </div>
         <div className="multi-fields">
-          <input type="text" placeholder='Zip Code'/>
-          <input type="text" placeholder='Country'/>
+          <input type="text" placeholder='Zip Code' />
+          <input type="text" placeholder='Country' />
         </div>
         <input type="text" placeholder='Phone' />
       </div>
       <div className="place-order-right">
-      <div className="cart-total">
+        <div className="cart-total">
           <h2>Cart Total</h2>
           <div>
             <div className="cart-total-details">
@@ -37,19 +40,19 @@ const PlaceOrder = () => {
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>${getTotalCartAmount()===0?0:5}</p>
+              <p>${getTotalCartAmount() === 0 ? 0 : 5}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+5}</b>
+              <b>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 5}</b>
             </div>
           </div>
-            <button onClick={()=>navigate('/food-delivery-application/order')}>Proceed to Check Out</button>
+          <button type="button" onClick={() => navigate('/food-delivery-application/')}>Proceed to Check Out</button>
         </div>
-        </div>
+      </div>
     </form>
-  )
-}
+  );
+};
 
-export default PlaceOrder
+export default PlaceOrder;
